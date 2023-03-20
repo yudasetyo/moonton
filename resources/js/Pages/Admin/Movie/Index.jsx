dd34a900b5743965f20a53bc9cae5880274c252e
yudasetyo <yudasetyo07@gmail.com>
2023-03-20 19:09:38 +0700

    Admin Store Movie

---
+++ resources/js/Pages/Admin/Movie/Index.jsx
@@ -0,0 +1,20 @@
+import Authenticated from "@/Layouts/Authenticated/Index";
+import PrimaryButton from "@/Components/PrimaryButton";
+import React from "react";
+import FlashMessage from "@/Components/FlashMessage";
+import { Link } from "@inertiajs/inertia-react";
+
+export default function Index({ auth, flashMessage }) {
+    return (
+        <Authenticated auth={auth}>
+            <Link href={route("admin.dashboard.movie.create")}>
+                <PrimaryButton type="button" className="w-40 mb-8">
+                    Insert New Movie
+                </PrimaryButton>
+            </Link>
+            {flashMessage?.message && (
+                <FlashMessage message={flashMessage.message} />
+            )}
+        </Authenticated>
+    );
+}
